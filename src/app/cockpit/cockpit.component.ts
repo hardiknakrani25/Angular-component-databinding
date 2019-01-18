726d10b732d88b0bef45426a09cde1ea51bd208b
Hardik Nakrani <hdnakarani@gmail.com>
2019-01-18 13:47:44 +0530

    Access to the Template  DOM with ViewChild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,8 +9,10 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // alias for output
   @Output('bpCreated') bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
   constructor() {
   }
 
@@ -20,14 +22,14 @@ export class CockpitComponent implements OnInit {
   onAddServer(serverNameInput: HTMLInputElement) {
  this.serverCreated.emit({
    serverName: serverNameInput.value,
-   serverContent: this.newServerContent
+   serverContent: this.serverContentInput.nativeElement.value,
  });
   }
 
   onAddBlueprint(serverNameInput: HTMLInputElement) {
     this.bluePrintCreated.emit({
       serverName: serverNameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
